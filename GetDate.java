77477eee0a204debd04ae8ea765e6b296d5c6218
Elfving2 <112498823+Elfving2@users.noreply.github.com>
2022-11-13 21:05:00 +0100

    Update GetDate.java

--- GetDate.java
+++ GetDate.java
@@ -4,15 +4,14 @@ import java.time.temporal.WeekFields;
 import java.util.Locale;
 
 public class GetDate {
-  public static String getDate (int x, String today){
-    LocalDate now = LocalDate.now();
-    TemporalField fieldISO = WeekFields.of(Locale.FRANCE).dayOfWeek();
-    LocalDate date = now.with(fieldISO, x);
-    String dateString = date.toString();
-    if (now.getDayOfMonth() == date.getDayOfMonth()) {
-      return "Today " + dateString;
+    public static String getDate (int x){
+        LocalDate now = LocalDate.now();
+        TemporalField fieldISO = WeekFields.of(Locale.FRANCE).dayOfWeek();
+        LocalDate date = now.with(fieldISO, x);
+        String dateString = date.toString();
+        if (now.getDayOfMonth() == date.getDayOfMonth()) {
+            return "Today " + dateString;
+        }
+        return dateString;
     }
-    return dateString;
-  }
-
 }
